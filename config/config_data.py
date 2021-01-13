73cc42144d16440fad0ad62cdc193ab01ac9e254
cherry951010 <13037521553@163.com>
2021-01-13 09:54:55 +0800

    first commit

---
+++ config/config_data.py
@@ -0,0 +1,3 @@
+'''配置文件'''
+accesstoken  = '2cd84376-cbee-4727-9e95-e1f2042dd8c1'
+tid = {'tid':''}
---
+++ testcases/test_topic.py
@@ -0,0 +1,5 @@
+import requests
+import pytest
+from config.config_data import accesstoken,tid
+
+'''获取主题首页'''
