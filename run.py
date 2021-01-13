94e270e3d8209159edbf182ed16d12ebc72c2c61
cherry951010 <13037521553@163.com>
2021-01-13 11:16:29 +0800

    优化报告： 添加时间

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 入口
 '''
 import pytest
+import time
 
 if __name__ == '__main__':
-    pytest.main(['testcases','-v','--html=report/reports.html'])
+    current_data = time.strftime('%Y-%m-%d %H:%M:%S')
+    pytest.main(['testcases','-v',f'--html=report/reports{current_data}.html'])
